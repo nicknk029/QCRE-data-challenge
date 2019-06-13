53c88f3c24c17e7c23a8d40c95b7213a65021170
Nirmalkumar Telagalapalli <51757248+nicknk029@users.noreply.github.com>
2019-06-13 10:14:50 -0500

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,10 @@
+
+####### IMPORT NECESSARY LIBRARIES #########
+
+import pandas as pd
+import numpy as np
+
+######## ASSIGN THE ORIGINAL EXCEL FILE TO raw_train DATAFRAME ##########
+
+raw_train = pd.read_excel('processminer-rareevent.xlsx')
+
